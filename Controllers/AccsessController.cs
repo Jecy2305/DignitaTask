a8da21a6da97aae6a4e6ff60c016e7606acea7af
Jecy2305 <Edu_Cordova1@hotmail.com>
2023-09-13 21:47:46 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AccsessController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DignitaTask.Models;
+
+namespace DignitaTask.Controllers
+{
+    public class AccsessController : Controller
+    {
+
+        static string conection = "Data Source = LAPTOP-BFK427HA\\SQL; Initial Catalog = DB_DIGNITA; Integrated Security = true"; 
+
+        // GET: Accsess
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Trabajador oUsuario)
+        {
+            using (SqlConnection cn = new SqlConnection(conection))
+            {
+                SqlCommand cmd = new SqlCommand("sp_ValidarUsuario", cn);
+                cmd.Parameters.AddWithValue("Correo", oUsuario.Email);
+                cmd.Parameters.AddWithValue("Clave", oUsuario.Password);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+                
+                oUsuario.Dni = (Int16)cmd.ExecuteScalar();
+            }
+
+            if (oUsuario.Dni != 0)
+            {
+                return RedirectToAction("Home","Contact"); 
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ Models/Trabajador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DignitaTask.Models
+{
+    public class Trabajador
+    {
+        public int Dni { get; set; }
+        public string Email { get; set; }
+        public string Password{ get; set; }
+        
+    }
+}
